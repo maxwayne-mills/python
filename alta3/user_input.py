bea69ed18e816eb59bf71bab60b3a8153443558c
Clarence Mills <clarence.mills@gmail.com>
2018-10-17 19:38:52 -0400

    Python script testing out user input, using input module

---
+++ alta3/user_input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+user_input = input('Please enter your name')
+print("Your name is:" + user_input)   # Print what's in the double quotes and concatenate (+) what stored within the variable user_input
+print(user_input)  # Print out what's contained within the variable
