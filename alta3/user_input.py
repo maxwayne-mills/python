290fdda61cfc54f46ee69d82f176a3d76ac8ee9b
Clarence Mills <clarence.mills@gmail.com>
2018-10-17 19:58:26 -0400

    Update comments

--- alta3/user_input.py
+++ alta3/user_input.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # Author: Clarence Mills
 
-# Collect user name and store and store within variable user_input
+# Collect user name and store within the variable user_input
 user_input = input('Please enter your name')
 
-# Print what's in the double quotes and concatenate (+) what stored within the variable user_input
+# Print contents withiin the double quotes and concatenate (+) what;s stored within the variable user_input
 print("Your name is:" + user_input) 
 
 # # Print out what's contained within the variable user_input, stored from above
