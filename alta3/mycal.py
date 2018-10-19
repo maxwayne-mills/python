1a004261bd222cb45a977b07e634e60da103d798
Clarence Mills <clarence.mills@gmail.com>
2018-10-19 08:50:54 -0400

    Display year and month as inputed from user

--- alta3/mycal.py
+++ alta3/mycal.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import calendar
 
-user_input = input('Enter the month to display')
+year_input = input('Enter year')
+month_input = input('Enter the month to display')
 
-lilcal = calendar.month(2018, user_input)
+lilcal = calendar.month(year_input, month_input)
 print("Here is a tiny calendar:")
 print(lilcal)
