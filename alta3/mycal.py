bf84e1dc1e18d6d49d977e5748545cb64f1c178c
Clarence Mills <clarence.mills@gmail.com>
2018-10-19 16:05:49 -0400

    print each month of the year based on user input

--- alta3/mycal.py
+++ alta3/mycal.py
@@ -4,10 +4,11 @@ import calendar
 year_input = input('Enter year')
 if year_input == '':
     year_input = 2018
+    print(year_input)
 
-print(year_input)
-month_input = int(input('Enter the month to display'))
+# month_input = int(input('Enter the month to display'))
 
-lilcal = calendar.month(year_input, month_input)
-print("Here is a tiny calendar:")
-print(lilcal)
+for x in range(1,13):
+    lilcal = calendar.month(year_input, x)
+    print("Calendar:")
+    print(lilcal)
