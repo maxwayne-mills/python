6eadb2d7a4597496edb2cf08d4abb9de646c5ea0
Clarence Mills <clarence.mills@gmail.com>
2018-10-19 16:16:44 -0400

    add comments

--- alta3/mycal.py
+++ alta3/mycal.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+
+# Module to display the month of the year
 import calendar
 
-year_input = input('Enter year')
+year_input = int(input('Enter year: '))   # Convert input to an integer
+
+# Check to ensure year_input variable has a value, else default to 2018
 if year_input == '':
     year_input = 2018
     print(year_input)
 
 # month_input = int(input('Enter the month to display'))
 
-for month in range(1,13):
-    lilcal = calendar.month(year_input, month)
+for month in range(1,13):   # Display all tweleve months, start at 1 as there is no '0' month
+    smallcal = calendar.month(year_input, month)
     print("Calendar:")
-    print(lilcal)
+    print(smallcal)
