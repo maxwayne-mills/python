b915e849b3a5e68c16f06bed5ecd4b3b60dbab60
Clarence Mills <clarence.mills@gmail.com>
2018-10-19 17:23:08 -0400

    verify year entered is a leap year and display in output

--- alta3/mycal.py
+++ alta3/mycal.py
@@ -3,16 +3,18 @@
 # Module to display the month of the year
 import calendar
 
+# Collect year from user input
 year_input = int(input('Enter year: '))   # Convert input to an integer
 
-# Check to ensure year_input variable has a value, else default to 2018
-if year_input == '':
-    year_input = 2018
-    print(year_input)
+# Check if the year is a leap year
+verify_leap_year = calendar.isleap(year_input)
 
-# month_input = int(input('Enter the month to display'))
+if verify_leap_year == False:
+    is_leap_year = "Not a leap year"
+else:
+    is_leap_year = "Leap year"
 
 for month in range(1,13):   # Display all tweleve months, start at 1 as there is no '0' month
     smallcal = calendar.month(year_input, month)
-    print("Calendar:")
+    print("Calendar: "  + is_leap_year )
     print(smallcal)
