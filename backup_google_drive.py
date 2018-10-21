e191f9c8ec8068bf413a418216ab2b11c14d1cc7
Clarence Mills <clarence.mills@gmail.com>
2018-10-20 22:25:53 -0400

    Backup google drive, hard coded paths

---
+++ backup_google_drive.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Clarence Mils
+
+''' Backup local Google to cloud ''' 
+
+import os
+import subprocess
+
+user_dir = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmills/Documents/google_drive_oss']
+
+for x in user_dir:
+    print('Backup up google drive ' + x )
+    os.chdir(user_dir[0])
+    #subprocess.Popen('make')
+    subprocess.call('make')
