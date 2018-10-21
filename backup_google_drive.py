bd76eed412c45fc81c4decb90fbd79134efa6d83
Clarence Mills <clarence.mills@gmail.com>
2018-10-21 12:17:58 -0400

    define new varialbes file_download_dest, dir_dest, file_dest

--- backup_google_drive.py
+++ backup_google_drive.py
@@ -5,13 +5,13 @@
 import os
 import subprocess
 
-user_dirs = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmills/Documents/google_drive_oss']
+backup_dirs = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmills/Documents/google_drive_oss']
 
 def main():
-    for x in user_dirs:
+    for backup_items in backup_dirs:
         subprocess.call('clear')
-        print('Backuping up google drive: ' + x + '\n')
-        os.chdir(user_dirs[0])
+        print('Backuping up google drive: ' + backup_items + '\n')
+        os.chdir(backup_dirs[0])
         #subprocess.Popen('make')
         subprocess.call('make')
 
---
+++ download_dropbox_app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Author: Clarence Mills
+''' Download python script to control dropbox daemon '''
+
+import urllib.request
+import os
+
+#  Define varialbes
+url = 'https://www.dropbox.com/download?dl=packages/dropbox.py' 
+file_download_dest = '/home/cmills/Downloads/dropbox.py'
+dir_dest = '/home/cmills/bin'
+file_dest = '/home/cmills/bin/dropbox.py'
+
+# Download Dropbox app
+urllib.request.urlretrieve(url,file_download_dest)
+
+# Create bin directory within user home.
+if not os.path.isdir(dir_dest):         #check if directory exists first
+    print('Creating bin directory')
+    os.mkdir(dir_dest)                  # Create directory
+else:
+    print(dir_dest + ' direectory exists')
+    os.rename(file_download_dest,file_dest) # move file to bin directory
