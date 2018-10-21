9b50691ccd12d44bd598b9e64193f05a9c065ce7
Clarence Mills <clarence.mills@gmail.com>
2018-10-20 22:27:12 -0400

    chagne variable user_dir to user_dirs

--- backup_google_drive.py
+++ backup_google_drive.py
@@ -6,10 +6,10 @@
 import os
 import subprocess
 
-user_dir = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmills/Documents/google_drive_oss']
+user_dirs = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmills/Documents/google_drive_oss']
 
-for x in user_dir:
+for x in user_dirs:
     print('Backup up google drive ' + x )
-    os.chdir(user_dir[0])
+    os.chdir(user_dirs[0])
     #subprocess.Popen('make')
     subprocess.call('make')
