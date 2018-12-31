95054f3ee27beecc39869f46478db839cd943f03
Clarence Mills <clarence.mills@gmail.com>
2018-12-30 23:43:04 -0500

    correct spelling in main function

--- backup_google_drive.py
+++ backup_google_drive.py
@@ -10,7 +10,7 @@ backup_dirs = ['/home/cmills/Documents/google_drive_clarence_mills','/home/cmill
 def main():
     for backup_items in backup_dirs:
         subprocess.call('clear')
-        print('Backuping up google drive: ' + backup_items + '\n')
+        print('Backing up google drive: ' + backup_items + '\n')
         os.chdir(backup_dirs[0])
         #subprocess.Popen('make')
         subprocess.call('make')
