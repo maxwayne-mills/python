64d18d8f9ac9d4177082d37df24d3afe3ca02707
Clarence Mills <clarence.mills@gmail.com>
2018-12-30 23:30:58 -0500

    replaced by open_link_in_browser.py

--- open_web_url.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import webbrowser
-
-browser = "google-chrome"
-web_site_url = "https://www.google.com"
-
-# Open a new browser
-webbrowser.get(browser).open_new(web_site_url)
