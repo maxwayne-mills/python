e84e8185de92540b6961c48c7ab724b738692b94
Clarence Mills <clarence.mills@gmail.com>
2019-03-24 17:49:29 -0400

    Major changes
    Comment section at the top of the file explains how to structure the the csv file. 3/4 of the code has been moved into the get_link function that is now called from the line that launches the web browser. While loop also continues to display the formatted output to the screen unless the user chooses not to.

--- open_link_in_browser.py
+++ open_link_in_browser.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
-''' Author: Clarence Mills '''
-''' Script to open a url contained within a file '''
+""" Author: Clarence Mills """
+""" Script to open a URL contained within a file """
+""" expects a csv file in a three column format, first field cateogory of the link, second field link, third field describes the link"""
 
 import webbrowser as wb
 import os
 import csv
 import sys
 
-os.popen("clear")   # Clear the screen
+def get_link():
+    os.popen("clear")  # Clear the screen
+    # Open csv file passed in from the command line as the first argument
+    csvfile = open(sys.argv[1], "r")
 
-# Open csv file passed in from the command line as the first argument
-csvfile = open(sys.argv[1],'r')
+    # Read in contents of the file
+    filereader = csv.reader(csvfile)
+    next(filereader)  # Skip the header
 
-# Read in contents of the file 
-filereader = csv.reader(csvfile)
-next(filereader)   # Skip the header
+    # Create a list of file contents
+    link_data = list(filereader)
 
-# Create a list of file contents
-linkdata = list(filereader)
+    n = 0
+    for row_in_file in link_data:
+        print(
+            n, row_in_file[1], row_in_file[2]
+        )  # Print line number and a row from the file
+        n = n + 1  # increment counter by 1
+    
+    linenum = int(input("Enter line number: ")
+    )  # convert string to an integer using the int function
 
-n = 0
-for row_in_file in linkdata:
-    print(n,row_in_file[1])     # Print line number and a row from the file
-    n = n + 1                   # increment counter by 1
+    # load link variable based on line number entered by user and selecting the second column
+    link = link_data[linenum][
+        1
+    ]  # number 1 indicates to use the second column that contains the link
 
-linenum = int(input("Enter line number: " )) # convert string to an integer using the int function
+    return link
 
-# load link variable based on line number entered by user and selecting the second column   
-link = linkdata[linenum][1]     # number 1 indicates to use the second column that contains the link
+# Open the link in Google chrome tab in incognito mode
+wb.get("google-chrome %s --incognito").open_new_tab(get_link())
 
-# Open the link in an incognito Google chrome tab
-wb.get('google-chrome %s --incognito').open_new_tab(link)
+user_input = input("continue: y or n ")
+while user_input != "n":
+    wb.get("google-chrome %s --incognito").open_new_tab(get_link())
+
